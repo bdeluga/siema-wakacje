86f02bfe0d64d86293e62f1e1cc8208d534b3dbb
Xewol <bartekdeluga0702@gmail.com>
2022-11-23 23:42:41 +0100

    need to create own typer sadly :(

---
+++ frontend/src/components/typer/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Typer = () => {
+  return <div>typer</div>;
+};
+
+export default Typer;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { type NextPage } from 'next'
-import Head from 'next/head'
+import { type NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
@@ -8,11 +8,14 @@ const Home: NextPage = () => {
         <title>Siema Wakacje</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-blue flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]  text-gray-100">
-        <div className="text-5xl">Szukasz [tutaj piszacy sie tekst] ? </div>
+      <main className="text-blue flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-indigo-700 to-indigo-900  text-gray-100">
+        <div className="text-5xl">
+          <span>Szukasz </span>
+          <span className="font-mono"></span>
+        </div>
         <div className="mt-10 flex  w-96 items-center justify-evenly">
           <input
-            type={'text'}
+            type={"text"}
             className="h-10 border bg-transparent pl-2 text-lg"
           />
           <button className=" flex h-12 items-center justify-center rounded-md bg-[#bfccf5] px-10 text-xl text-gray-800">
@@ -21,7 +24,7 @@ const Home: NextPage = () => {
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
