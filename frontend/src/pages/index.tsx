cd84caf8322fcc3a3e93e04d1740ef16b645c91f
Xewol <bartekdeluga0702@gmail.com>
2022-11-27 00:42:30 +0100

    moved typer to its own hook

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Typer from "../components/typer";
+import Typer from "../components/Typer";
 
 const Home: NextPage = () => {
   return (
@@ -9,14 +9,14 @@ const Home: NextPage = () => {
         <title>Siema Wakacje</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-blue flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-indigo-500 to-indigo-800  text-gray-100">
-        <div className="text-[7rem]">
-          <span>{"Szukasz... "}</span>
-          <Typer texts={["pokoju?", "restauracji?,trasy rowerowej?"]} />
-          <p className="ml-4 text-3xl font-bold text-gray-800 underline underline-offset-2">
+      <main className="text-blue flex min-h-screen w-screen flex-col items-center justify-center bg-gradient-to-br from-indigo-500 to-indigo-800  text-gray-100">
+        <h1 className="w-full p-32 text-[7rem]">
+          Szukasz...&nbsp;
+          <Typer />
+          <p className="ml-4 text-3xl font-bold text-gray-800">
             My to znajdziemy.
           </p>
-        </div>
+        </h1>
         <div className="mt-20 flex w-full items-center justify-center gap-40 ">
           <input
             type={"text"}
