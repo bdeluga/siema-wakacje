d74e6445271d026881574ded70c9826e48bbf7b0
Xewol <79754698+Xewol@users.noreply.github.com>
2022-12-05 23:52:50 +0100

    rudy zjeb
    zakomentował klucz...

--- backend/siema_wakacje_b/siema_wakacje_b/settings.py
+++ backend/siema_wakacje_b/siema_wakacje_b/settings.py
@@ -32,7 +32,6 @@ environ.Env.read_env()
 
 DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-z9!@q_iq!&*bu17hdf1lu+83tjy0=dzz8atdaon=nfo9rardyt'
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = []
