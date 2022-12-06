b6bd159ef7aa2bb313a8e1fd077353ba0251f2b9
Maciej <76630179+HAHAHAHAHAXDDD@users.noreply.github.com>
2022-12-06 01:26:27 +0100

    return hotel names for city

--- backend/siema_wakacje_b/pages/views.py
+++ backend/siema_wakacje_b/pages/views.py
@@ -4,31 +4,31 @@ from django.http import HttpResponse, JsonResponse
 import csv
 import os
 import json
+import osmnx as ox
+import pandas
 
 def cityPageView(request):
 
-    # empty endpoint
-    if str(request.path[1:]) == '':
+    # city name from path
+    city = str(request.path)[1:]
+    # prevent to pass favicon.ico or empty string to nominatim request
+    if city == '' or city == 'favicon.ico':
         return HttpResponse(status=200)
-    
-    
-    # absolute path cuz data is in .csv outside the project
-
-    worldCities = os.path.join(settings.DATA_DIR, 'worldcities.csv')
-
-    # open stream
-    # take every row from data file and split it to take only city column
-    # [1:-1] cuz data is in "cityName" format so need to remove marks
-    # take only city name and compare with endpoint
-    # if finded status = 200 (OK)
-    # else status = 404 (ERROR)
-
-    with open(worldCities, encoding='utf8') as data:
-        for row in data:
-            if ((row.split(',')[0])[1:-1]).upper() == (str(request.path)[1:]).upper():
-                return HttpResponse(status=200)
-    return HttpResponse(status=404)
-
+    # search for hotels only
+    tags = {'amenity': 'hotel',
+        'building': 'hotel',
+        'tourism': 'hotel'}
+    # geodataframe with response
+    hotelResponse = ox.geometries_from_place(city, tags=tags)
+    # dictionary for clean data to return
+    hotels = {'hotels' : []}
+    # iterate over geodataframe and append names to respone dictionary
+    for ind in hotelResponse.index:
+        hotel = {}
+        hotel['name'] = hotelResponse['name'][ind]
+        hotels['hotels'].append(hotel)
+    # response as json
+    return JsonResponse(hotels)
 
 
 def cityQueryView(request):
