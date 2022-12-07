076fe91ee337c29efc65d6f3468701c8772678d1
Xewol <bartekdeluga0702@gmail.com>
2022-12-07 13:00:10 +0100

    added 404

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function FourOhFour() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-900 text-slate-100">
+      <h1 className="text-4xl ">404 - Page Not Found</h1>
+    </div>
+  );
+}
