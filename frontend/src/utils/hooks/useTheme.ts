549e82b84ab7f0c102acbc41a0cceb7867d0ee9c
Xewol <bartekdeluga0702@gmail.com>
2023-03-03 20:07:28 +0100

    refactored

--- frontend/src/utils/hooks/useTheme.ts
+++ frontend/src/utils/hooks/useTheme.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export type Theme = "light" | "dark" | "system";
-type resolvedTheme = "light" | "dark";
+type ResolvedTheme = "light" | "dark";
 
 interface ThemeClassNames {
   light: string;
@@ -10,58 +10,50 @@ interface ThemeClassNames {
 
 const useTheme = ({ light, dark }: ThemeClassNames) => {
   const [theme, setTheme] = useState<Theme>("system");
-  const [resolvedTheme, setResolvedTheme] = useState<resolvedTheme>("light");
   const MEDIA = "(prefers-color-scheme: dark)";
   const storageKey = "theme";
   const attr = "data-theme";
 
   const resolveTheme = (theme: Theme) => {
-    let themeToResolve;
     switch (theme) {
       case "dark":
-        themeToResolve = "dark";
-        break;
+        return "dark";
       case "light":
-        themeToResolve = "light";
-        break;
+        return "light";
       case "system":
-        const prefersDark = window.matchMedia(MEDIA).matches;
-        themeToResolve = prefersDark ? "dark" : "light";
-        break;
+        return window.matchMedia(MEDIA).matches ? "dark" : "light";
     }
-
-    return themeToResolve as resolvedTheme;
   };
 
   //Initial run
   useEffect(() => {
     const storedTheme = localStorage.getItem(storageKey);
-    if (storedTheme) setTheme(storedTheme as Theme);
-    if (!storedTheme || storedTheme === "system") {
-      if (!storedTheme) localStorage.setItem(storageKey, "system");
-      const prefersDark = window.matchMedia(MEDIA).matches;
-      if (prefersDark) {
-        setResolvedTheme(prefersDark ? "dark" : "light");
-        document.documentElement.className = prefersDark ? "dark" : "light";
-        document.documentElement.setAttribute(attr, prefersDark ? dark : light);
-      }
+    //check if there is actually theme stored by user
+    const initialTheme =
+      storedTheme && storedTheme !== "system"
+        ? (storedTheme as Theme)
+        : undefined;
+    const prefersDark = window.matchMedia(MEDIA).matches;
+    const systemTheme = prefersDark ? "dark" : "light";
+
+    const resolvedTheme = resolveTheme(initialTheme || systemTheme);
+    document.documentElement.className = resolvedTheme;
+    document.documentElement.setAttribute(
+      attr,
+      resolvedTheme === "dark" ? dark : light
+    );
+
+    if (initialTheme) {
+      setTheme(initialTheme);
     }
   }, [dark, light]);
 
   //   every switch
   useEffect(() => {
-    console.log(theme);
+    localStorage.setItem(storageKey, theme);
+    document.documentElement.className = resolveTheme(theme);
+    document.documentElement.setAttribute(attr, resolveTheme(theme));
   }, [theme]);
-
-  // useEffect(() => {
-  //     setResolvedTheme(resolveTheme(theme));
-  //     document.documentElement.className = resolvedTheme;
-  //     document.documentElement.setAttribute(
-  //       attr,
-  //       resolvedTheme === "light" ? light : dark
-  //     );
-  //     localStorage.setItem(storageKey, resolvedTheme);
-  //   }, [theme, resolvedTheme, light, dark]);
 
   return { theme, setTheme };
 };
