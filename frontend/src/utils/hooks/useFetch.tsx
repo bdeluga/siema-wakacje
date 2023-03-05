0ba3687027e4d619bf7eabe7c4363aab82328906
Xewol <bartekdeluga0702@gmail.com>
2023-03-05 15:14:04 +0100

    better useFetch

--- frontend/src/utils/hooks/useFetch.tsx
+++ frontend/src/utils/hooks/useFetch.tsx
@@ -1,29 +1,15 @@
 import { useState } from "react";
-import axios, { type AxiosError } from "axios";
-import type { FetchResponse } from "@/types";
+import axios from "axios";
 
 export const useFetch = () => {
-  const [res, setRes] = useState<FetchResponse | null>(null);
   const [isFetching, setIsFetching] = useState(false);
 
   const fetch = (url: string) => {
     setIsFetching(true);
-    axios
-      .get(url, { timeout: 5000 })
-      .then((res) => {
-        setRes({
-          data: res.data,
-        });
-      })
-      .catch((err: AxiosError) => {
-        setRes({
-          error: err,
-        });
-      })
-      .finally(() => {
-        setIsFetching(false);
-      });
+    return axios.get(url, { timeout: 5000 }).finally(() => {
+      setIsFetching(false);
+    });
   };
 
-  return { fetch, res, isFetching };
+  return { fetch, isFetching };
 };
