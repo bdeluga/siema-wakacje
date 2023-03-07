ac81ed614d5e09d6913f412c7be47f4fd071a650
Xewol <bartekdeluga0702@gmail.com>
2023-03-08 00:55:48 +0100

    ??

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -14,6 +14,7 @@ type Props = {
 const Map = ({ position, markers }: Props) => {
   return (
     <MapContainer
+      //@ts-expect-error idk man leaflet sucks
       center={[position.lat, position.lng]}
       zoom={12}
       scrollWheelZoom={true}
