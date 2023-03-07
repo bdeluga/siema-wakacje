ed4f69afa0b5fe9437d0d78cb4348a0996a0a9c1
Xewol <bartekdeluga0702@gmail.com>
2023-03-07 17:56:23 +0100

    simple react-query

--- frontend/src/server/api/routers/example.ts
+++ frontend/src/server/api/routers/example.ts
@@ -18,4 +18,13 @@ export const exampleRouter = createTRPCRouter({
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
   }),
+
+  fetch: publicProcedure.input(z.string()).query(async ({ input }) => {
+    const res = await fetch(`http://127.0.0.1:8000/${input}`);
+    return res.json() as Promise<{
+      hotels: {
+        name: string;
+      }[];
+    }>;
+  }),
 });
