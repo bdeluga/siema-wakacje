270846c587972a3945069b8c3940601ecb5c0410
Xewol <bartekdeluga0702@gmail.com>
2023-03-07 17:55:49 +0100

    working on memo Map

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -1,23 +1,33 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { memo } from "react";
 
-type Coords = {
+interface Coords {
   lat: number;
   lng: number;
+}
+type Props = {
+  position: Coords;
+  markers?: Coords[];
 };
 
-const Map = ({ lat, lng }: Coords) => {
+const Map = ({ position, markers }: Props) => {
   return (
     <MapContainer
-      center={[lat, lng]}
+      center={[position.lat, position.lng]}
       zoom={12}
       scrollWheelZoom={true}
       zoomControl={false}
       className="h-full w-full"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {markers?.map((marker, idx) => (
+        <Marker key={idx} position={[marker.lat, marker.lng]} />
+      ))}
     </MapContainer>
   );
 };
 
-export default Map;
+const MemoizedMap = memo(Map);
+
+export default MemoizedMap;
