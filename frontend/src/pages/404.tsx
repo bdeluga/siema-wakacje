a3c7fd4fb1ee72b0d58c72a86dec3b4e5922688e
Xewol <bartekdeluga0702@gmail.com>
2023-03-09 08:55:09 +0100

    changed 404

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,7 +1,26 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+import Link from "next/link";
+
 export default function FourOhFour() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-slate-900 text-slate-100">
-      <h1 className="text-4xl ">404 - Page Not Found</h1>
-    </div>
+    <>
+      <Head>
+        <title>404</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="form-control flex h-screen flex-col">
+        <Header />
+        <div className="flex flex-1 flex-col   items-center justify-center">
+          <h1 className="text-6xl font-bold">404</h1>
+          <p className="mt-10 text-2xl font-bold">
+            Page not found. Try again later.
+          </p>
+          <Link className="link mt-4" href="/">
+            Go back
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
