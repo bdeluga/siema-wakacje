d44fe421509eb149e75ae0b56cb5f48a5c8bce12
Xewol <bartekdeluga0702@gmail.com>
2023-03-14 00:57:43 +0100

    hotel type

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -1,7 +1,16 @@
-export type City = {
+export interface City {
   name: string;
   country: string;
   iso: string;
   lat: number;
   lng: number;
-};
+}
+
+export interface Hotel {
+  name: string;
+  rate: number;
+  point: {
+    lat: number;
+    lng: number;
+  };
+}
