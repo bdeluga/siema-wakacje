35627e770d2cdff7128b03f54c799d574c052375
Xewol <bartekdeluga0702@gmail.com>
2023-03-14 18:57:04 +0100

    QueryButons

---
+++ frontend/src/components/QueryButtons/index.tsx
@@ -0,0 +1,48 @@
+import { useQueryKeyStore } from "@/useStore";
+import React from "react";
+const QueryButtons = () => {
+  const { queryKey, setKey } = useQueryKeyStore();
+
+  return (
+    <div className="mx-auto mt-2 flex space-x-4">
+      <button
+        id="hotels"
+        className={`btn rounded ${
+          queryKey === "hotels" ? "btn-primary" : "btn-secondary"
+        }`}
+        onClick={(e) => setKey(e.currentTarget.id)}
+      >
+        noclegi
+      </button>
+      <button
+        id="recreations"
+        className={`btn rounded ${
+          queryKey === "recreations" ? "btn-primary" : "btn-secondary"
+        }`}
+        onClick={(e) => setKey(e.currentTarget.id)}
+      >
+        rekreacja
+      </button>
+      <button
+        id="history"
+        className={`btn rounded ${
+          queryKey === "history" ? "btn-primary" : "btn-secondary"
+        }`}
+        onClick={(e) => setKey(e.currentTarget.id)}
+      >
+        historia
+      </button>
+      <button
+        id="restaurants"
+        className={`btn rounded ${
+          queryKey === "restaurants" ? "btn-primary" : "btn-secondary"
+        }`}
+        onClick={(e) => setKey(e.currentTarget.id)}
+      >
+        restauracje
+      </button>
+    </div>
+  );
+};
+
+export default QueryButtons;
