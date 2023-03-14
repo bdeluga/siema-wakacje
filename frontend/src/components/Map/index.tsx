2e26170d81875a7a764bb2f8f874322f628b8c2e
Xewol <bartekdeluga0702@gmail.com>
2023-03-14 18:56:46 +0100

    fallback when no markers

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -3,6 +3,8 @@ import "leaflet/dist/leaflet.css";
 import type { Point } from "@/utils/types";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
   position: Point;
@@ -10,6 +12,14 @@ type Props = {
 };
 
 export default function Map({ position, markers }: Props) {
+  if (!markers)
+    return (
+      <div className="flex flex-col  gap-2 text-slate-100">
+        <FontAwesomeIcon className="fa-spin text-4xl" icon={faCircleNotch} />
+        <span>Wczytywanie mapy...</span>
+      </div>
+    );
+
   return (
     <MapContainer
       center={[position.lat, position.lng]}
